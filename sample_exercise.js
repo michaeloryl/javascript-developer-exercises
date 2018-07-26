05081a31113e442e9d77afbb724e10f3d799f8e5
Michael Oryl <Oryl.Michael@pennmutual.com>
2018-07-26 13:11:13 -0400

    Added an actual sample exercise file for literal people

---
+++ sample_exercise.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+  // add code here that will take the name param and
+  // say hello in the form of "Hello, <Name_Here>!" to the console
+  console.log('Hello, %s!', name)
+}
+
+sayHello('Michael')
+
+// Expected output to the console when run with "node sample_exercise.js" would be:
+// Hello, Michael!
