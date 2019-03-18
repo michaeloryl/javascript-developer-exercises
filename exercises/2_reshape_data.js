6d111fc4fde01d3201d3b964fb4ada259c17753b
Ian Rockefeller <aaaairock@gmail.com>
2019-03-17 21:40:21 -0400

    Exercise 2 Success!

--- exercises/2_reshape_data.js
+++ exercises/2_reshape_data.js
@@ -68,7 +68,10 @@ function flattenRideshareArray(array) {
     initialArray into an array that is equal to expectedArray
     */
     // PLACE YOUR CODE BELOW
-
+    return {
+      rideShareId: element.rideShareId,
+      numberMembers: element.members.length
+    }
     // PLACE YOUR CODE ABOVE
   })
 
